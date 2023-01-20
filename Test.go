a792979748e5e5c06772be636a8f73bc9ea9467a
Maxime Herry <maxime@herry.cc>
2023-01-20 16:28:00 +0100

    produit matrices
    Co-authored-by: MargauxMsl <MargauxMsl@users.noreply.github.com>

--- Test.go
+++ Test.go
@@ -1,26 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
 
-func multiplie(i int,j int, a[][]int,b[][]int,r[][]int,c chan int,) {
+)
+
+const TAILLEMAT=2
+
+func multiplie(i int, j int,a *[][]int,b*[][]int,c chan int) int{
 	mult := 0
-	for k := 0; k < len(a); k++ {
-		r[i][j] += a[i][k]*b[k][j]
+	for k := 0; k < len(*a); k++ {
+		mult += (*a)[k][i]*(*b)[j][k]
 	}
-	c <- mult // send mult to c
+	return mult
+	//c <- mult // send mult to c
+}
+func max(a int, b int) int {
+	if (a>b){
+		return a
+	}
+	return b
+
+	
+}
+
+func colonne(a[][]int,index int,) []int{
+	column := make([]int, len(a))
+    for i := range a {
+        column[i] = a[i][index]
+    }
+    return column
+
 }
 
 func main() {
-	a := [][]int{{2,2,2},{2,2,2}}	
-	b := [][]int{{2,2,2},{2,2,2}}
-	 r [2][3]int
-	c := make(chan int)
+	a:= [][]int{}
+	b:= [][]int{}
+	for i := 0; i < TAILLEMAT; i++ {
+		rowa := make([]int, TAILLEMAT)
+		rowb := make([]int, TAILLEMAT)
+		for j := 0; j < TAILLEMAT; j++ {
+			rowa[j] =rand.Intn(10) + 1
+			rowb[j] =rand.Intn(10) + 1
+		}
+		a= append(a, rowa)
+		b= append(b, rowb)
+
+	}
+	result := [][]int{}
+	for i :=  range a {
+		row := make([]int, len(a))
+		for j :=  range b[0] {
+			row[j] = (i+1)/(i+1)-1
+		}
+		result = append(result, row)
+	}
+	
+	fmt.Println(a)
+	fmt.Println(b)
+	
 	for i := 0; i < len(a); i++ {
-		for j := 0; j < len(a); j++ {
-			go multiplie(i,j,a,b,r,c)
+			for j := 0; j < len(a[0]); j++ {
+				result[i][j]= multiplie(i,j,&a,&b)
 		}
 	}
-	fmt.Println(r)
+	fmt.Println(result)
+	
 
 } 
 
